7a71798adbc4d73a2bf0f390e21978b9640949f0
aluisaoliveira <aluisaoliveira@ipvc.pt>
2024-06-05 02:11:29 +0100

    entities

---
+++ DBLayer/Entities/Carrinho.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ESOF.WebApp.DBLayer.Entities;
+
+public class Carrinho
+{
+    public Guid CarrinhoId { get; set; }
+    
+    [Required]
+    public string Nome { get; set; }
+    
+    [Required]
+    public DateTime Data { get; set; }
+    
+    [Required]
+    public double Total { get; set; }
+    
+    [Required]
+    public double PrecoUnitario { get; set; }
+
+    public ICollection<User> Users { get; set; }
+    public ICollection<Produto> Produtos { get; set; }
+    
+}
---
+++ DBLayer/Entities/Categoria.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ESOF.WebApp.DBLayer.Entities;
+
+public class Categoria
+{
+    public Guid CategoriaId { get; set; }
+    
+    [Required]
+    public string Nome { get; set; }
+}
