c1669a8d3f40a30c9f97de1de83b02f8ce31730c
aluisaoliveira <aluisaoliveira@ipvc.pt>
2024-06-05 02:16:18 +0100

    entities

---
+++ DBLayer/Entities/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ESOF.WebApp.DBLayer.Entities;
+
+public class Produto
+{
+    public Guid ProdutoId { get; set; }
+    
+    [Required]
+    public string Nome { get; set; }
+    
+    [Required]
+    public string Descricao { get; set; }
+    
+    [Required]
+    public double Preco { get; set; }
+    
+    [Required]
+    public int QuantidadeStock { get; set; }
+    
+    public ICollection<Categoria> Categorias { get; set; }
+}
